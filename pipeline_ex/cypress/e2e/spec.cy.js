8ff086460dd2bdf32633dffdcca26aa54480ffce
sonia-lu <lukaszewicz.sonia@gmail.com>
2023-06-06 15:02:36 +0200

    TEST RUN

--- pipeline_ex/cypress/e2e/spec.cy.js
+++ pipeline_ex/cypress/e2e/spec.cy.js
@@ -1,30 +1,27 @@
-describe('Pipeline spec', async() => {
+describe("Pipeline spec", async () => {
+  it("test docker compose", async () => {
+    expect(1).to.be.eq(1);
+    cy.visit("/hello");
+  });
 
+  it("tp1", async () => {
+    expect(1).to.be.eq(1);
 
-    it('tp1', async() => {
-        expect(1).to.be.eq(1)
+    cy.visit("https://example.cypress.io");
+  });
+  it("tp2", async () => {
+    expect(1).to.be.eq(1);
 
-        cy.visit('https://example.cypress.io')
+    cy.visit("https://example.cypress.io");
+  });
+  it("tp3", async () => {
+    expect(1).to.be.eq(0);
 
-    })
-    it('tp2', async() => {
+    cy.visit("https://example.cypress.io");
+  });
+  it("tp4", async () => {
+    expect(1).to.be.eq(1);
 
-        expect(1).to.be.eq(1)
-
-        cy.visit('https://example.cypress.io')
-
-    })
-    it('tp3', async() => {
-
-        expect(1).to.be.eq(0)
-
-        cy.visit('https://example.cypress.io')
-
-    })
-    it('tp4', async() => {
-
-        expect(1).to.be.eq(1)
-
-        cy.visit('https://example.cypress.io')
-    })
-})
+    cy.visit("https://example.cypress.io");
+  });
+});
